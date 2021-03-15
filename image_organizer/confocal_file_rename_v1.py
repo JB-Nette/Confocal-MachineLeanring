3f9bd4bb5c68ddb93649e99722099a287f28bd80
lehui123 <65656404+lehui123@users.noreply.github.com>
2021-03-15 13:11:44 +0800

    Include user input for bleaching buffer name and add conditions to only rename first cycle if there is no bleaching cycle

--- image_organizer/confocal_file_rename_v1.py
+++ image_organizer/confocal_file_rename_v1.py
@@ -1,4 +1,4 @@
-        # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Fri Mar  5 10:19:15 2021
 
@@ -20,7 +20,7 @@ def spaceremoval(path):
     Remove all spaces in file name
     """
     for f in os.listdir(path):       
-        os.rename(os.path.join(path,f),os.path.join(path,f.replace(' ','')))
+        os.rename(os.path.join(path, f), os.path.join(path, f.replace(' ', '')))
         
    
 def imagingrounddetection(path):
@@ -30,9 +30,9 @@ def imagingrounddetection(path):
     """
     # confocal image file name follows a pattern: 1st cycle (name)_F(ii).ims,
     # 2nd cycle (name)_1_F(ii).ims  and so on.  
-    #2nd cycles onward image file pattern     
+    # 2nd cycles onward image file pattern
     ims_pattern = re.compile(r"([a-zA-Z0-9_-]+)_(\d+)_F(\d+).(ims)")
-    #1st cycle image file pattern
+    # 1st cycle image file pattern
     ims_pattern1 = re.compile(r"([a-zA-Z0-9_-]+)_F(\d+).(ims)")
     
     pattern = []
@@ -41,8 +41,8 @@ def imagingrounddetection(path):
     final_pattern = []
     spaceremoval(path)
     for f in os.listdir(path):
-        match = re.match(ims_pattern,f)
-        match1 = re.match(ims_pattern1,f)
+        match = re.match(ims_pattern, f)
+        match1 = re.match(ims_pattern1, f)
         if match:
             if match.group(1) not in pattern:
                 pattern.append(match.group(1))
@@ -55,6 +55,7 @@ def imagingrounddetection(path):
         final_pattern = pattern1
 
     return final_pattern
+
 
 def spaceremovalxml(path):
     """
@@ -70,31 +71,32 @@ def spaceremovalxml(path):
             match = re.match(xml_pattern, f)
 
             if match:
-                xtree = et.parse(os.path.join(path,f))
+                xtree = et.parse(os.path.join(path, f))
                 xroot = xtree.getroot()
                 for node in xroot:
                     if node.tag == 'STACKS':
                         for i in node:
                             d = i.attrib
-                            for k,v in d.items():
+                            for k, v in d.items():
                                 if k == 'IMG_REGEX':
-                                    v = v.replace(' ','')
+                                    v = v.replace(' ', '')
                                     i.attrib[k] = v
-                xtree.write(os.path.join(path,f))
+                xtree.write(os.path.join(path, f))
         for f in os.listdir(path):
-            match1 = re.match(xml1_pattern,f)                
+            match1 = re.match(xml1_pattern, f)
             if match1:
-                xtree = et.parse(os.path.join(path,f))
+                xtree = et.parse(os.path.join(path, f))
                 xroot = xtree.getroot()
                 for node in xroot:
                     if node.tag == 'STACKS':
                         for i in node:
                             d = i.attrib
-                            for k,v in d.items():
+                            for k, v in d.items():
                                 if k == 'IMG_REGEX':
-                                    v = v.replace(' ','')
+                                    v = v.replace(' ', '')
                                     i.attrib[k] = v
-                xtree.write(os.path.join(path,f))   
+                xtree.write(os.path.join(path, f))
+
 
 def propernaming(path):
     """
@@ -122,34 +124,35 @@ def propernaming(path):
                 all_rename[f] = new_f
     
         for f in os.listdir(path):   
-            match1 = re.match(xml_pattern,f)
+            match1 = re.match(xml_pattern, f)
 
             if match1:
                 new_f = match1.group(1) + '_0.' + match1.group(2)
                 all_rename[f] = new_f
                 try:
-                    xtree = et.parse(os.path.join(path,f))
+                    xtree = et.parse(os.path.join(path, f))
                     xroot = xtree.getroot()
                     for node in xroot:
                         if node.tag == 'STACKS':
                             for i in node:
                                 d = i.attrib
-                                for k,v in d.items():
+                                for k, v in d.items():
                                     if k == 'IMG_REGEX':
                                         v = all_rename[v]
                                         i.attrib[k] = v
-                    xtree.write(os.path.join(path,f))   
+                    xtree.write(os.path.join(path, f))
                 except KeyError:
                     pass
         for f in os.listdir(path):           
-            match2 = re.match(txt_pattern,f)                    
+            match2 = re.match(txt_pattern, f)
             if match2:
                 new_f = match2.group(1) + '_0_metadata.' + match2.group(2)
                 all_rename[f] = new_f
-        for k,v in all_rename.items():
-            os.rename(os.path.join(path,k),os.path.join(path,v))
+        for k, v in all_rename.items():
+            os.rename(os.path.join(path, k), os.path.join(path, v))
 
-def findbleach(xml_file,bleaching_buffer):
+
+def findbleach(xml_file, bleaching_buffer):
     """
     
 
@@ -168,14 +171,12 @@ def findbleach(xml_file,bleaching_buffer):
     nodes = []
     
     for node in xroot:
-        for i,e in enumerate(node):
-            n_list = []
-            n_list.append(e.tag)
-            n_list.append(e.text)
+        for i, e in enumerate(node):
+            n_list = [e.tag, e.text]
             nodes.append(n_list)
     img_index = []
     bleach_index = []
-    for i,e in enumerate(nodes):
+    for i, e in enumerate(nodes):
         if bleaching_buffer in e:
             bleach_index.append(i)
         if 'DAFusion' in e:
@@ -189,13 +190,13 @@ def findbleach(xml_file,bleaching_buffer):
             pass
         else:
             for j in hyb_index:
-                if j > base and j < img_index[i+1]:
+                if base < j < img_index[i + 1]:
                     bleach.append(i+1)
             base = img_index[i+1]
     return bleach
 
 
-def namereplacement(bleach_id,path,new_path):
+def namereplacement(bleach_id, path, new_path):
     """
 
     Parameters
@@ -217,35 +218,35 @@ def namereplacement(bleach_id,path,new_path):
         ims_pattern = re.compile(r'(' + str(pat) + ')_(\d+)_F(\d+).(ims)')
         txt_pattern = re.compile(r'(' + str(pat) + ')_(\d+)_metadata.(txt)')
         for f in os.listdir(path):
-           match = re.match(ims_pattern, f)
-           match1 = re.match(xml_pattern,f)
-           match2 = re.match(txt_pattern,f)
-           if match:
-               for i,bleach in enumerate(bleach_id):
+            match = re.match(ims_pattern, f)
+            match1 = re.match(xml_pattern, f)
+            match2 = re.match(txt_pattern, f)
+            if match:
+                for i, bleach in enumerate(bleach_id):
                     if int(match.group(2)) == int(bleach):
-                        new_f = match.group(1) + '_' + match.group(2) + '_bleached_F'+ match.group(3) + '.' + match.group(4)                        
+                        new_f = match.group(1) + '_' + match.group(2) + '_bleached_F' + match.group(3) + '.' + match.group(4)
                         all_rename[f] = new_f
                     if int(match.group(2)) == int(bleach)-1:
                         new_f = f
                         all_rename[f] = new_f
-           if match1:
-               for i,bleach in enumerate(bleach_id):
+            if match1:
+                for i, bleach in enumerate(bleach_id):
                     if int(match1.group(2)) == int(bleach):
                         new_f = match1.group(1) + '_' + match1.group(2) + '_bleached.' + match1.group(3)
                         all_rename[f] = new_f
                     if int(match1.group(2)) == int(bleach)-1:
                         new_f = f
                         all_rename[f] = new_f
-           if match2:
-               for i,bleach in enumerate(bleach_id):
+            if match2:
+                for i, bleach in enumerate(bleach_id):
                     if int(match2.group(2)) == int(bleach):
                         new_f = match2.group(1) + '_' + match2.group(2) + '_bleached_metadata.' + match2.group(3)
                         all_rename[f] = new_f
                     if int(match2.group(2)) == int(bleach)-1:
                         new_f = f
                         all_rename[f] = new_f
-        for k,v in all_rename.items():
-            shutil.copy(os.path.join(path,k),os.path.join(new_path,v))         
+        for k, v in all_rename.items():
+            shutil.copy(os.path.join(path, k), os.path.join(new_path, v))
         input('Renaming is done. Press Enter To Exit')
         exit(0)
 
@@ -254,22 +255,22 @@ def main():
     root = tk.Tk()
     root.withdraw()
     path = filedialog.askdirectory(title="Please select image data directory")
-    xml_file = filedialog.askopenfilename(title = 'Please choose your Davil script file')
+    xml_file = filedialog.askopenfilename(title='Please choose your Davil script file')
     bleaching_buffer = simpledialog.askstring('Bleaching Buffer', 'Please input your bleaching buffer name from your xml script. Cancel if there is none')
     root.destroy()
-    if bleaching_buffer == None:
+    if bleaching_buffer is None:
         spaceremovalxml(path)
         propernaming(path)
         input('There is no bleaching cycle, only 1st cycle is renamed. Press Enter To Exit')
         exit(0)
     else:
-        new_path = path +'/Renamed'
+        new_path = path + '/Renamed'
         if not os.path.isdir(new_path):
             os.makedirs(new_path)
         spaceremovalxml(path)
         propernaming(path)
         bleach_id = findbleach(xml_file, bleaching_buffer)
-        namereplacement(path,new_path,bleach_id)
+        namereplacement(path, new_path, bleach_id)
 
 
 
